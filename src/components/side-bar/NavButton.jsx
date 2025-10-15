fbaeb7743a958168da0677ae5b73470c2008b365
CenturyOrb <122176094+CenturyOrb@users.noreply.github.com>
2025-10-15 18:24:19 -0400

    Finished Sidebar

--- src/components/side-bar/NavButton.jsx
+++ src/components/side-bar/NavButton.jsx
@@ -1,12 +1,21 @@
 import styles from './navbutton.module.css'
+import { motion } from 'motion/react'
 
-const NavButton = ({data}) => { 
+const NavButton = ({data, changeSelected}) => { 
 	const highlight = data.selected
 		? styles.highlight
 		: null;
 
 	return(
-		<li className={`${styles.nav_button} ${highlight}`}>{data.content}</li>	
+		<motion.li 
+			className={`${styles.nav_button} ${highlight}`}
+			onClick={changeSelected}
+			whileHover={{ scale: 1.02 }}
+  			whileTap={{ scale: 0.95 }}
+		>
+			{data.icon && <data.icon size={24} style={{ marginRight: '8px' }} />}
+			<span style={{verticalAlign: '0'}}>{data.content}</span>
+		</motion.li>	
 	);
 }
 
--- src/components/side-bar/SideBar.jsx
+++ src/components/side-bar/SideBar.jsx
@@ -4,13 +4,25 @@ import globalStyles from '../globalStyles.module.css'
 import { Colors } from '../../constants.js'
 
 import NavButton from './NavButton.jsx'
+import { MdOutlineInventory2 } from "react-icons/md";
+import { RxDashboard } from "react-icons/rx";
+import { IoSettingsOutline } from "react-icons/io5";
 
 const SideBar = () => { 
 	const [navData, setNavData] = useState([
-		{content: 'Dashboard', selected: true},
-		{content: 'Inventory', selected: false},
-		{content: 'Summary', selected: false}	
+		{ content: 'Dashboard', selected: true, icon: RxDashboard },
+		{ content: 'Inventory', selected: false, icon: MdOutlineInventory2 },
+		{ content: 'Setting', selected: false, icon: IoSettingsOutline}	
 	]);
+
+	const changeCurrentSelected = (i) => { 
+		setNavData(
+			prevNavData => 
+			prevNavData.map((item, j) => 
+				({ ...item, selected: j === i })
+			)
+		);	
+	}
 
 	return (
 		<section className={styles.side_bar}>
@@ -21,10 +33,14 @@ const SideBar = () => {
 						<NavButton 
 							key={index} 
 							data={data}
+							changeSelected={() => changeCurrentSelected(index)}
 						/>
 					))}
 				</ul>
 			</nav>
+			<div className={styles.user_container}> 
+				John Doe	
+			</div>
 		</section>
 	);
 }

ac05a98f3fd1f9005c6957541fbd149869378b7e
CenturyOrb <122176094+CenturyOrb@users.noreply.github.com>
2025-10-22 23:04:14 -0400

    Laid out recent updates

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,9 +1,11 @@
 import styles from './main.module.css'
 import { Colors } from '../../constants.js'
-import { IoSearch } from "react-icons/io5";
-import { FaRegBell } from "react-icons/fa";
-import { CiFileOn } from "react-icons/ci";
-import { PiStackSimple } from "react-icons/pi";
+import { IoSearch } from 'react-icons/io5'
+import { FaRegBell } from 'react-icons/fa'
+import { CiFileOn } from 'react-icons/ci'
+import { PiStackSimple } from 'react-icons/pi'
+import { HiOutlineRectangleStack } from 'react-icons/hi2'
+import { MdAttachMoney } from 'react-icons/md'
 
 import SearchBar from '../search-bar/SearchBar.jsx'
 import Container from '../container/Container.jsx'
@@ -23,29 +25,35 @@ const Main = () => {
 			<h2 style={
 				{color: Colors.primary_light,
 				fontFamily: 'Poppins, sans-serif',
-				letterSpacing: '1.5px'}
+				letterSpacing: '1.5px',
+				marginTop: '1rem'}
 			}>
 				Dashboard
 			</h2>	
 			
 			{/* general summary of items and boxes */}
-			<section className={styles.categories}>
+			<section className={styles.dashboard_content}>
 				<div className={styles.summary}>
 					<Container customStyles={styles.item_summary}>	
 						<CiFileOn size={20}/>
 						Items
 					</Container>
 					<Container customStyles={styles.box_summary}>	
-						<PiStackSimple size={20} />
-                    	Inventory
+						<PiStackSimple size={20}/>
+                    	Containers
                     </Container>
 					<Container customStyles={styles.inventory_summary}>	
+						<HiOutlineRectangleStack size={20}/>
                     	Inventory
                     </Container>
 					<Container customStyles={styles.revenue_summary}>	
+						<MdAttachMoney size={20}/>
                     	Revenue
                     </Container>
 				</div>
+				<Container customStyles={styles.recent_updates}>
+					Recent updates goes here
+				</Container>
 			</section>
 		</main>
 	);

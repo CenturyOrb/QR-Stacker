1d3859dc7e03e930570915deb110a16d612ef1db
CenturyOrb <122176094+CenturyOrb@users.noreply.github.com>
2025-10-23 23:19:38 -0400

    Added qr.code to packages

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,11 +1,13 @@
 import styles from './main.module.css'
 
 import DashboardSummary from './dashboard/DashboardSummary.jsx'
+import QRCodeGenerator from '../qrcode/QRCodeGenerator.jsx'
 
 const Main = () => {
 	return(
 		<main className={styles.main}>
 			<DashboardSummary />
+			<QRCodeGenerator text={'hi'}/>
 		</main>
 	);
 }

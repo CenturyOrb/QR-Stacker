3187ad0e1bf392ab7b22c6f9ba8e9a3d73b92a30
CenturyOrb <122176094+CenturyOrb@users.noreply.github.com>
2025-11-06 23:20:39 -0500

    added middleware to read data from requsts

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,16 @@ import express from 'express'
 const app = express()
 const port = 3000
 
+// middleware to parse form data and http req body data to req.body
+app.use(
+	express.urlencoded({extended: true}), 
+	express.json()
+);
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  	res.json({user: 'Crownless'})
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  	console.log(`Example app listening on port ${port}`)
 })

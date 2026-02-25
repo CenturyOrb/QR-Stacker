c969d4f00e6bab214f3bcf2c34553c588173be40
CenturyOrb <122176094+CenturyOrb@users.noreply.github.com>
2026-02-25 13:24:26 -0500

    Added user resource directory functionality

--- backend/src/repositories/authRepo.js
+++ backend/src/repositories/authRepo.js
@@ -1,7 +1,19 @@
 import prisma from '../config/db.js'
+import fs from "fs";
+import path from "path";
 
 export async function upsertUser(user) { 
 	const { uid, email, name } = user;
+
+	const existingUser = await prisma.user.findUnique({
+    	where: { uid }
+  	});
+	
+	// mkdir for user files
+	if (!existingUser) {
+  		const userDir = path.join("resources/user", uid);
+  		fs.mkdirSync(userDir, { recursive: true });
+	}
 	return await prisma.user.upsert({
     	where: { uid },
     	update: { email, name },
